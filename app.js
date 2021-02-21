59473ad4ac951581fede5f1eacbf0b9e945eb7b0
unknown <juliocnl92@gmail.com>
2021-02-21 14:54:43 -0700

    mostrar estados

--- app.js
+++ app.js
@@ -2,15 +2,19 @@
 graf = d3.select('#graf')
 
 ancho_total = graf.style('width').slice(0, -2)
-alto_total = ancho_total * 0.42
+alto_total = ancho_total * 0.8
+
+//alto_total  = graf.style('height')//.slice(0, 0)
+//ancho_total = alto_total * 1
+//alert(alto_total)
 
 graf.style('width' , `${ ancho_total}px`)
     .style('height', `${ alto_total }px`)
 
-margins = {  top:    ancho_total*0.06 
-           , left:   ancho_total*0.06
-           , right:  ancho_total*0.06
-           , bottom: ancho_total*0.18 }
+margins = {  top:    ancho_total * 0.06 
+           , left:   ancho_total * 0.06
+           , right:  ancho_total * 0.06
+           , bottom: ancho_total * 0.18 }
 
 ancho = ancho_total - margins.left -  margins.right
 alto  = alto_total  - margins.top  - margins.bottom
@@ -20,93 +24,83 @@ svg = graf.append('svg')
           .style('width', `${ ancho_total }px`)
           .style('height', `${ alto_total }px`);
 
-g = svg.append('g')
+dataArray = []
+
+function agregar_grupo(){
+  g = svg.append('g')
          .style('transform', `translate(${margins.left}px, ${margins.top}px)`)
          .style('width', ancho + 'px')
          .style('height', alto + 'px');
-
-dataArray = []
+  return g;
+}
 
 //    3.2  funciones para ajustar los datos de entrada a la representacion de salida
 function dominio(escala,minEntrada,maxEntrada,minSalida,maxSalida) {
   f = escala
-        .range( [minSalida ,  maxSalida])
-        .domain([minEntrada, maxEntrada])
+    .range( [minSalida ,  maxSalida])
+    .domain([minEntrada, maxEntrada])
   return f
 }
 function preprocesar(data) {
-  console.log(data)
+  //console.log(data)
+  min_x = Math.min.apply(null, data.map((e) => Math.min.apply(null, e.coordenadas.map((p) => p[0]))))
+  max_x = Math.max.apply(null, data.map((e) => Math.max.apply(null, e.coordenadas.map((p) => p[0]))))
+  min_y = Math.min.apply(null, data.map((e) => Math.min.apply(null, e.coordenadas.map((p) => p[1]))))
+  max_y = Math.max.apply(null, data.map((e) => Math.max.apply(null, e.coordenadas.map((p) => p[1]))))
 
-  max_Y = Math.max.apply(null, data.map(function(d) { return d.cordenadas; }))
-  fy = dominio(d3.scaleLinear(),0,max_Y,0,alto)
+  fy = dominio(d3.scaleLinear(),min_y,max_y,0, alto)
+  fx = dominio(d3.scaleTime()  ,min_x,max_x,0,ancho)  
 
-  fx = dominio(d3.scaleTime(),0,data.length-1,0,ancho)    
-/*
-  data.forEach((d,i) => {
-    d.indice = i + 1;
-    d.dia    = fx(d.indice);
-    d.total  =  d.total=="" ? "": alto - fy(d.total);  
-    dia  = d.fecha.substring(0, 2);
-    mes  = d.fecha.substring(3, 5);
-    anio = d.fecha.substring(6, 10);
-    d.fecha = new Date(anio, mes-1, dia);
-  });
-
-  data.forEach((d,i) => {
-    d.tendencia =  mediaMovil(data,i);
-  });
-  */
-
+  for (i=0; i < data.length; i+=1) {
+    for (j=0; j < data[i].coordenadas.length; j+=1) {
+      data[i].coordenadas[j][0] = fx(data[i].coordenadas[j][0])
+      data[i].coordenadas[j][1] = alto-fy(data[i].coordenadas[j][1])
+    }
+  }
+  
   return data;
 }
 
-function random(d){
-  proporcion=d/ancho;
-  numero = Math.random();
-  return proporcion*proporcion*numero;
-}
-function duracion(d, maximo){
-  maximo     = document.getElementById('limite').max;
-  random     = 700 + Math.random() * 1000;
-  proporcion = d.dia / maximo;
-  return proporcion * random;
-}
 //  3.3  funciones para dibujar los diferentes grupos de componentes
 function render() {
-
+  data = dataArray
+  dibujarMapa(data)
 }
 
-//    3.4  funciones para dibujar componenentes individuales
-function renderizarPath(color, puntos){
-  puntos = puntos.map(function(d) { return [d[0]*ancho_total,d[1]*ancho_total]});
-  
-  elementos = svg.append('path')
-    .attr('d', d3.line()(puntos))
-    .attr('stroke', color)
-    .attr('stroke-width', '2px')
-    .attr('fill', 'none');
-  
-  return elementos;
-}
+function dibujarMapa(data){
 
+  indicador  = document.getElementById('indicadores').value;
+  //alert(indicador);
+  minc = Math.min.apply(null, data.map((e) => e[indicador]))
+  maxc = Math.max.apply(null, data.map((e) => e[indicador]))
+
+  var fc = d3.scaleQuantize()
+    .domain([minc,maxc])
+    .range(["#65e800", "#e0e800", "#e8aa00", "#e85d00", "#e80000"]);
+
+  data.forEach(estado => dibujarArea(estado,fc(estado[indicador])));
+}
+function dibujarArea(estado,color){
+  agregar_grupo().selectAll("polygon")
+    .data([estado.coordenadas])
+    .enter()
+    .append("polygon")
+    .attr("points",(d) => d)
+    .attr("stroke", "black")
+    .attr("fill", color)
+    .attr("stroke-width", 2);
+}
 
 // 4. Carga de datos
-/*
-d3.csv('estados.json')
-  .then(function(data) {
-    console.log(data.json())
-    //console.log(JSON.parse(data))
-    //dataArray = preprocesar(data);
-    render();
-  })
-  .catch(e => {
-    console.log('No se tuvo acceso al archivo: ' + e.message)
-  });
-*/
 fetch('estados.json')
   .then(response => response.json())
   .then(data =>{
     dataArray = preprocesar(data);
     render();
   });
+
+function actualizar(){
+  render();
+  //alert("sdfsdf");
+}
 
