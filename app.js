ebf5d75f195dfb14a2f92a096cf7b01b5b202e13
unknown <juliocnl92@gmail.com>
2021-02-21 23:42:55 -0700

    me quede en linea 120

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 // 1. Configuración
-graf = d3.select('#graf')
+grafMapa = d3.select('#grafMapa')
+grafBarras = d3.select('#grafBarras')
 
-ancho_total = graf.style('width').slice(0, -2)
+ancho_total = grafMapa.style('width').slice(0, -2)
 alto_total = ancho_total * 0.8
 
-//alto_total  = graf.style('height')//.slice(0, 0)
-//ancho_total = alto_total * 1
-//alert(alto_total)
-
-graf.style('width' , `${ ancho_total}px`)
-    .style('height', `${ alto_total }px`)
+grafMapa.style('width' , `${ ancho_total}px`).style('height', `${ alto_total }px`)
+grafBarras.style('width' , `${ ancho_total}px`).style('height', `${ alto_total }px`)
 
 margins = {  top:    ancho_total * 0.06 
            , left:   ancho_total * 0.06
@@ -20,29 +17,25 @@ ancho = ancho_total - margins.left -  margins.right
 alto  = alto_total  - margins.top  - margins.bottom
 
 // 2. Variables globales
-svg = graf.append('svg')
-          .style('width', `${ ancho_total }px`)
-          .style('height', `${ alto_total }px`);
-
+svgM =   grafMapa.append('svg').style('width', `${ ancho_total }px`).style('height', `${ alto_total }px`);
+svgB = grafBarras.append('svg').style('width', `${ ancho_total }px`).style('height', `${ alto_total }px`);
 dataArray = []
-g_estados = []
+grupos = []
 
-function agregar_grupo(){
+function agregar_g(svg){
   g = svg.append('g')
-         .style('transform', `translate(${margins.left}px, ${margins.top}px)`)
-         .style('width', ancho + 'px')
-         .style('height', alto + 'px');
+    .style('transform', `translate(${margins.left}px, ${margins.top}px)`)
+    .style('width', ancho + 'px')
+    .style('height', alto + 'px');
   return g;
 }
-function generar_g_estados(){
-  dataArray.forEach(estado => g_estados.push({indice:estado.indice, g:agregar_grupo()}));
+function generar_g(){
+  dataArray.forEach(estado => grupos.push({indice:estado.indice, gM:agregar_g(svgM), gB:agregar_g(svgB)}));
 }
 
 //    3.2  funciones para ajustar los datos de entrada a la representacion de salida
-function dominio(escala,minEntrada,maxEntrada,minSalida,maxSalida) {
-  f = escala
-    .range( [minSalida ,  maxSalida])
-    .domain([minEntrada, maxEntrada])
+function dominio(escala,minEntrada,maxEntrada, salida) {
+  f = escala.domain([minEntrada, maxEntrada]).range(salida)
   return f
 }
 function preprocesar(data) {
@@ -52,8 +45,8 @@ function preprocesar(data) {
   min_y = Math.min.apply(null, data.map((e) => Math.min.apply(null, e.coordenadas.map((p) => p[1]))))
   max_y = Math.max.apply(null, data.map((e) => Math.max.apply(null, e.coordenadas.map((p) => p[1]))))
 
-  fy = dominio(d3.scaleLinear(),min_y,max_y,0, alto)
-  fx = dominio(d3.scaleTime()  ,min_x,max_x,0,ancho)  
+  fy = dominio(d3.scaleLinear(),min_y,max_y,[0, alto])
+  fx = dominio(d3.scaleTime()  ,min_x,max_x,[0,ancho])  
 
   for (i=0; i < data.length; i+=1) {
     data[i].indice = i;
@@ -71,27 +64,26 @@ function preprocesar(data) {
 function render() {
   data = dataArray;
   dibujarMapa(data);
+  dibujarBarras(data);
 }
 
 function dibujarMapa(data){
-  indicador  = document.getElementById('indicadores').value;
+  indicador = document.getElementById('indicadores').value;
 
-  minc = Math.min.apply(null, data.map((e) => e[indicador]));
-  maxc = Math.max.apply(null, data.map((e) => e[indicador]));
+  min = Math.min.apply(null, data.map((e) => e[indicador]));
+  max = Math.max.apply(null, data.map((e) => e[indicador]));
 
-  var fc = d3.scaleQuantize()
-    .domain([minc,maxc])
-    .range(["#65e800", "#e0e800", "#e8aa00", "#e85d00", "#e80000"]);
+  fc = dominio(d3.scaleQuantize(),min,max,["#65e800", "#e0e800", "#e8aa00", "#e85d00", "#e80000"])
 
   data.forEach(estado => dibujarArea(estado,fc,indicador));
 }
 function dibujarArea(estado,fc,indicador){
   
-  g_estado = g_estados.find(g => g.indice == estado.indice);
+  grupo = grupos.find(g => g.indice == estado.indice);
   
   color = fc(estado[indicador]);
 
-  elementos = g_estado.g.selectAll('polygon').data(estado.coordenadas)
+  elementos = grupo.gM.selectAll('polygon').data(estado.coordenadas)
 
   elementos.enter()
     .append("polygon")
@@ -109,13 +101,42 @@ function dibujarArea(estado,fc,indicador){
     .remove();
   
 }
+function dibujarBarras(data){
+  indicador = document.getElementById('indicadores').value;
+
+  min = Math.min.apply(null, data.map((e) => e[indicador]));
+  max = Math.max.apply(null, data.map((e) => e[indicador]));
+
+  datos = data.map(estado => [estado.nombre,estado[indicador]]);
+  //falta ordenar datos
+  fx = dominio(d3.scaleLinear(),min,max,[[0,ancho]])
+
+  elementos = grupo.gB.selectAll('bar').data(datos)
+
+  elementos.enter()
+    .append("rect")
+    .style("fill", "steelblue")
+    .attr("x", function(e) { return 0; })
+    .attr("width", function(d) { return  20*fx(d[1]);} )
+    .attr("y", function(d,i) { return i*20; })
+    .attr("height", 15)
+    .merge(elementos)
+    .transition()
+    .duration(d => 1000)
+    .attr("fill", color);
+
+  elementos.exit()
+    .transition()
+    .remove();
+
+}
 
 // 4. Carga de datos
 fetch('estados.json')
   .then(response => response.json())
   .then(data =>{
     dataArray = preprocesar(data);
-    generar_g_estados();
+    generar_g();
     actualizar();
   });
 
