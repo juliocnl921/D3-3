f87a9c9e93b9a7cf85a8e3ebfb9eb629a87a8477
unknown <juliocnl92@gmail.com>
2021-02-22 19:49:47 -0700

    horizontal

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ alto_total = ancho_total * 0.8
 grafMapa.style('width' , `${ ancho_total}px`).style('height', `${ alto_total }px`)
 grafBarras.style('width' , `${ ancho_total}px`).style('height', `${ alto_total }px`)
 
-margins = {  top:    ancho_total * 0.06 
+margins = {  top:    ancho_total * 0.1
            , left:   ancho_total * 0.10
-           , right:  ancho_total * 0.06
-           , bottom: ancho_total * 0.18 }
+           , right:  ancho_total * 0.10
+           , bottom: ancho_total * 0.12 }
 
 ancho = ancho_total - margins.left -  margins.right
 alto  = alto_total  - margins.top  - margins.bottom
@@ -29,18 +29,15 @@ dataArray = [];
 g_estados = [];
 g_barras  = null;
 label_y_g = null;
-eje_x     = d3.axisBottom();
-eje_y     = d3.axisLeft();
 eje_x_g   = null;
 eje_y_g   = null;
 
-fy_b = d3.scaleLinear()
+fy_b = d3.scaleBand()
   .range([alto, 0]);
 
-fx_b = d3.scaleBand()
+fx_b = d3.scaleLinear()
   .range([0, ancho]);
 
- 
 function agregar_g(padre,x,y){
   g = padre.append('g')
     .style('transform', `translate(${x}px, ${y}px)`)
@@ -50,10 +47,9 @@ function agregar_g(padre,x,y){
 }
 function generar_g(){
   dataArray.forEach(estado => g_estados.push({indice:estado.indice, g:agregar_g(svgM,margins.left,margins.top)}));
-  g_barras = agregar_g(svgB,margins.left,margins.top);
-  eje_x_g =agregar_g(g_barras,0,alto);
-  eje_y_g = agregar_g(g_barras,0,0);
-  
+  g_barras  = agregar_g(svgB, 1.6*margins.left, margins.top);
+  eje_x_g   = agregar_g(svgB, 1.6*margins.left, 0.15 * alto);
+  eje_y_g   = agregar_g(svgB, 1.6*margins.left, margins.top);
   label_y_g = agregar_g(svgB,0,margins.top);
 }
 
@@ -127,51 +123,50 @@ function dibujarBarras(data){
 
   datos = data
     .map(estado => [estado.nombre, estado[indicador]])
-    .sort((a,b) => (a[1] < b[1]) ? 1 : ((b[1] < a[1]) ? -1 : 0));
+    .sort((a,b) => (a[1] > b[1]) ? 1 : ((b[1] > a[1]) ? -1 : 0));
 
   min = Math.min.apply(null, data.map((e) => e[indicador]));
   max = Math.max.apply(null, data.map((e) => e[indicador]));
   ajuste = ((max-min)*0.1);
   min = (min-ajuste)<0 ? 0: (min-ajuste);
 
-  fy_b.domain([min, max]);
-  fx_b.domain(datos.map(d => d[0]));
+  fx_b.domain([min, max]);
+  fy_b.domain(datos.map(d => d[0]));
 
   elementos = g_barras.selectAll('rect').data(datos)
-
   elementos.enter()
     .append('rect')   
     .style('fill', COLOR_BASE)
-    .style('x', (d,i) => fx_b(d[0]) + 'px')
-    .style('y', d => fy_b(d[1])+'px')
-    .style('width' , (5)+'px')
-    .style('height', d => (alto - fy_b(d[1])) + 'px')
+    .style('x', (d,i) => 0 + 'px')
+    .style('y',     d => fy_b(d[0]) + 'px')
+    .style('width', d => fx_b(d[1])+  'px')
+    .style('height', 3+ 'px')
     .merge(elementos)
     .transition()
     .duration(DURACION)
-    .style('x', (d,i) => fx_b(d[0]) + 'px')
-    .style('y', d => fy_b(d[1])+'px')
-    .style('width' , fx_b.bandwidth()-2+'px')
-    .style('height', d => (alto - fy_b(d[1])) + 'px')
-
+    .style('x', (d,i) => 0 + 'px')
+    .style('y',     d => fy_b(d[0]) + 'px')
+    .style('width', d => fx_b(d[1])+  'px')
+    .style('height',8+ 'px')
   elementos.exit().transition().remove()
+  
 
-  yAxisCall = d3.axisLeft(fy_b)
-    
+  yAxisCall = d3.axisLeft(fy_b) 
   eje_y_g.transition()
     .duration(DURACION)
     .call(yAxisCall)
 
-  xAxisCall = d3.axisBottom(fx_b)
+  xAxisCall = d3.axisTop(fx_b)
   eje_x_g.transition()
     .duration(DURACION)
     .call(xAxisCall)
-    .selectAll('text')
-    .attr('x', '-8px')
-    .attr('y', '-5px')
-    .attr('text-anchor', 'end')
-    .attr('transform', 'rotate(-90)')
+    //.selectAll('text')
+    //.attr('x', '-8px')
+    //.attr('y', '-5px')
+    //.attr('text-anchor', 'end')
+    //.attr('transform', 'rotate(-90)')
 
+  /*
   texto = label_y_g.selectAll('text').data([texto_indicador])
 
   texto.enter()
@@ -191,6 +186,7 @@ function dibujarBarras(data){
       .text(d => d);
       
   texto.exit().transition().remove()
+  */
 }
 
 //--------------------------------------
